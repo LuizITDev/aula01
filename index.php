46155ddaf7c63a76e1902af663678fa6091209ac
LuizITDev <luiz.job@outlook.com>
2025-10-28 22:05:26 -0300

    Primeira Aula com PHP

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+echo"hello World<br>";
+echo"<h1> SENAC </h1>";
+
+echo"Neymar<br>" ;
+
+
+$nome = "Dembele";
+$idade = 55;
+$altura = 1.90;
+$matrculado = false;
+echo "$nome<br> $idade<br>  $altura<br> $matrculado<br>";
+
+
+
+$multiplicacao=10*10;
+$divisao= 10/10;
+$soma=10+10;
+$subtracao=10-5;
+
+echo "$multiplicacao<br> $divisao<br> $soma<br> $subtracao<br>";
+echo"2 elevado a 6 : ".pow(2,6);
+
+?>
