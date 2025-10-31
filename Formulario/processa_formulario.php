c34307d2d9a70f6de460684746cacf8274eab920
LuizITDev <luiz.job@outlook.com>
2025-10-30 21:50:58 -0300

    aula php formulario

---
+++ Formulario/processa_formulario.php
@@ -0,0 +1,33 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$dataNasc = $_POST['dataNasc'];
+$telefone = $_POST['telefone'];
+$genero = $_POST['genero'];
+$curso = $_POST['curso'];
+$mensagem =$_POST['mensagem'];
+
+
+
+//EXIBIR
+
+echo "<h1> Dados Recebidos <h1>";
+echo "$nome<br>";
+echo "$email<br>";
+echo "$senha<br>";
+echo "$dataNasc<br>";
+echo "$telefone<br>";
+echo "$genero<br>";
+echo "$curso<br>";
+
+
+
+
+
+
+
+
+
+?>
